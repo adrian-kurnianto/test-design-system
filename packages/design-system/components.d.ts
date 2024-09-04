0819f825729aee59b1400a424972cd7445896657
adrian-kurnianto <kurnianto.adrian23@gmail.com>
2024-09-05 03:29:28 +0700

    setup github workflows

--- packages/design-system/components.d.ts
+++ packages/design-system/components.d.ts
@@ -9,5 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     MyButton: typeof import('./src/components/MyButton/MyButton.vue')['default']
+    Test: typeof import('./src/components/MyButton/test.vue')['default']
   }
 }
